81f6ca1b7f69273fe7ce0b51180c7973f4eac5b3
Damian Lobos <damianlobos24@gmail.com>
2024-01-18 14:23:56 -0300

    Basic movement and jump

--- 2DGame/Assets/Scripts/PlayerController.cs
+++ 2DGame/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D playerRB;
+    [SerializeField]
+    private float moveSpeed;
+    private float inputX;
+    [SerializeField]
+    private float jumpForce;
+
+    private void Awake()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        inputX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        playerRB.velocity = new Vector2(inputX, playerRB.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+        }
     }
 }
